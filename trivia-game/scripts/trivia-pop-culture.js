07a54ea7d7ad93112e4e611634a8e823a0111285
Arica Miller <89560782+bearmatainize@users.noreply.github.com>
2023-04-18 21:36:47 -0400

    Various Updates- Rules, Additional Trivia, etc

---
+++ trivia-game/scripts/trivia-pop-culture.js
@@ -0,0 +1,346 @@
+const questions = [
+    {
+        question: "What day do Star Wars fans celebrate National Star Wars Day?",
+        answer1: "July 21st",
+        answer2: "September 1st",
+        answer3: "May 4th",
+        answer4: "October 3rd",
+        correctAnswer: "answer3"
+    },
+
+    {
+        question: "What singer holds the record for most Grammy nominations?",
+        answer1: "Beyoncé",
+        answer2: "Stevie Wonder",
+        answer3: "Georg Solti",
+        answer4: "Jay-Z",
+        correctAnswer: "answer1"
+    },
+
+    {
+        question: "Who is the longest-serving host of The View",
+        answer1: "Joy Behar",
+        answer2: "Ana Navarro",
+        answer3: "Whoopi Goldberg",
+        answer4: "Sara Haines",
+        correctAnswer: "answer1"
+    },
+
+   {
+        question: "Who holds the record for winning the most Oscars?",
+        answer1: "Katharine Hepburn",
+        answer2: "Walt Disney",
+        answer3: "Meryl Streep",
+        answer4: "Jack Nicholson",
+        correctAnswer: "answer2"
+    },
+
+    {
+        question: "Who is the most followed person on Instagram?",
+        answer1: "Kylie Jenner",
+        answer2: "LeBron James",
+        answer3: "Cristiano Ronaldo",
+        answer4: "Taylor Swift",
+        correctAnswer: "answer3"
+    },
+
+    {
+        question: "What is the most watched YouTube video of all time?",
+        answer1: "Gangnam Style",
+        answer2: "Despacito",
+        answer3: "Baby Shark Dance",
+        answer4: "Charlie Bit My Finger",
+        correctAnswer: "answer3"
+    },
+
+    {
+        question: "How old was the youngest Oscar winner of all time?",
+        answer1: "14",
+        answer2: "10",
+        answer3: "18",
+        answer4: "12",
+        correctAnswer: "answer2"
+    },
+
+    {
+        question: "How many actors have played Batman onscreen?",
+        answer1: "5",
+        answer2: "8",
+        answer3: "10",
+        answer4: "11",
+        correctAnswer: "answer4"
+    },
+
+    {
+        question: "What animal is Indiana Jones afraid of in the movies?",
+        answer1: "Horses",
+        answer2: "Snakes",
+        answer3: "Cats",
+        answer4: "Scorpions",
+        correctAnswer: "answer2"
+    },
+
+    {
+        question: "How many Twilight books are there?",
+        answer1: "2",
+        answer2: "3",
+        answer3: "4",
+        answer4: "5",
+        correctAnswer: "answer3"
+    },
+
+    {
+        question: "Which Kardashian sister owns a denim brand called Good American?",
+        answer1: "Kim",
+        answer2: "Kourtney",
+        answer3: "Khloé",
+        answer4: "Kendall",
+        correctAnswer: "answer3"
+    },
+
+    {
+        question: "When is Mean Girls Day?",
+        answer1: "May 23rd",
+        answer2: "October 3rd",
+        answer3: "June 6th",
+        answer4: "September 16th",
+        correctAnswer: "answer2"
+    },
+
+    {
+        question: "On The Mandalorian, what is Baby Yoda's real name?",
+        answer1: "Yoder",
+        answer2: "Yaddle",
+        answer3: "Dobbu",
+        answer4: "Grogu",
+        correctAnswer: "answer4"
+    },
+
+    {
+        question: "What was Raven-Symoné's last name in That's So Raven?",
+        answer1: "Baxter",
+        answer2: "Williams",
+        answer3: "Hart",
+        answer4: "Gooden",
+        correctAnswer: "answer1"
+    },
+
+    {
+        question: "How many Jersey Shore cast members have competed on Dancing with the Stars?",
+        answer1: "2",
+        answer2: "3",
+        answer3: "5",
+        answer4: "7",
+        correctAnswer: "answer2"
+    },
+
+    {
+        question: "What insurance company has had a fictional spokesperson named Flo since 2008?",
+        answer1: "State Farm",
+        answer2: "Progressive",
+        answer3: "Geico",
+        answer4: "Farmers",
+        correctAnswer: "answer2"
+    },
+
+    {
+        question: "What movie won the first ever Oscar for Best Animated Film?",
+        answer1: "Shrek",
+        answer2: "Finding Nemo",
+        answer3: "The Incredibles",
+        answer4: "Spirited Away",
+        correctAnswer: "answer1"
+    },
+
+    {
+        question: "What is the highest grossing rom-com of all time?",
+        answer1: "Hitch",
+        answer2: "My Big Fat Greek Wedding",
+        answer3: "Pretty Woman",
+        answer4: "Love Actually",
+        correctAnswer: "answer2"
+    },
+
+    {
+        question: "Who won RuPaul's Drag Race All Stars Season 3?",
+        answer1: "Trixie Mattel",
+        answer2: "Alaska",
+        answer3: "Kennedy Davenport",
+        answer4: "Shangela",
+        correctAnswer: "answer1"
+    },
+
+    {
+        question: "Which character has the most screen time in the Lord of the Rings franchise?",
+        answer1: "Frodo",
+        answer2: "Gandalf",
+        answer3: "Legolas",
+        answer4: "Gollum",
+        correctAnswer: "answer1"
+    },
+
+    {
+        question: "Who has the most followers on TikTok?",
+        answer1: "Bella Poarch",
+        answer2: "Khabane Lame",
+        answer3: "Charli D'Amelio",
+        answer4: "MrBeast",
+        correctAnswer: "answer2"
+    },
+
+    {
+        question: "Who is the host of Wheel of Fortune?",
+        answer1: "Alex Trebek",
+        answer2: "Pat Sajak",
+        answer3: "Steve Harvey",
+        answer4: "Bob Barker",
+        correctAnswer: "answer2"
+    },
+
+   {
+        question: "How many Stars Wars movies are there (not including spin-offs like Rogue One:A Star Wars Story and Solo)",
+        answer1: "5",
+        answer2: "7",
+        answer3: "9",
+        answer4: "12",
+        correctAnswer: "answer3"
+    },
+
+    {
+        question: "How many years has Jeopardy been on air as of 2023?",
+        answer1: "10",
+        answer2: "64",
+        answer3: "59",
+        answer4: "45",
+        correctAnswer: "answer3"
+    },
+
+    {
+        question: "What year was Ratatouille released?",
+        answer1: "2007",
+        answer2: "2003",
+        answer3: "2009",
+        answer4: "2006",
+        correctAnswer: "answer1"
+    },
+
+    {
+        question: "What’s the longest running animated TV show?",
+        answer1: "The SImpsons",
+        answer2: "SpongeBob SquarePants",
+        answer3: "Pokemon",
+        answer4: "South Park",
+        correctAnswer: "answer1"
+    },
+
+    {
+        question: "Who was the first Disney princess?",
+        answer1: "Snow White",
+        answer2: "Cinderella",
+        answer3: "Persephone",
+        answer4: "Aurora",
+        correctAnswer: "answer3"
+    },
+
+    {
+        question: "Who was Walt Disney’s favorite princess?",
+        answer1: "Aurora",
+        answer2: "Ariel",
+        answer3: "Snow White",
+        answer4: "Cinderella",
+        correctAnswer: "answer4"
+    },
+
+    {
+        question: "Who is the first Disney princess to have a child?",
+        answer1: "Tiana",
+        answer2: "Belle",
+        answer3: "Jasmine",
+        answer4: "Ariel",
+        correctAnswer: "answer4"
+    },
+
+    {
+        question: "In what movie did Irving Berlin’s song “White Christmas” make its debut?",
+        answer1: "It’s a Wonderful Life",
+        answer2: "Holiday Inn",
+        answer3: "Miracle on 34th Street",
+        answer4: "A Charlie Brown Christmas",
+        correctAnswer: "answer2"
+    },
+
+    {
+        question: "Who was the first actor to portray James Bond on screen?",
+        answer1: "Sean Connery",
+        answer2: "Barry Nelson",
+        answer3: "Roger Moore",
+        answer4: "David Niven",
+        correctAnswer: "answer2"
+    },
+
+    {
+        question: "What’s the name of the ice cream parlor Steve and Robin work at in season three of Stranger Things?",
+        answer1: "Sundae School",
+        answer2: "I Scream",
+        answer3: "The Creamery",
+        answer4: "Scoops Ahoy!",
+        correctAnswer: "answer4"
+    }
+
+]
+
+let shuffledQuestions = questions //array to hold shuffled questions
+
+function handleQuestions() {
+    //function to shuffle questions and push to shuffledQuestion array
+    let currentIndex = shuffledQuestions.length, randomIndex;
+
+    //While there remain elements to shuffle
+    while (currentIndex != 0) {
+        //Pick a random element
+        randomIndex = Math.floor(Math.random() * currentIndex);
+        currentIndex--;
+
+        //Swap it with the current element
+        [shuffledQuestions[currentIndex], shuffledQuestions[randomIndex]] = [shuffledQuestions[randomIndex], shuffledQuestions[currentIndex]];
+    }
+    indexNumber = 0
+    nextQuestion(0)
+}
+
+//function for displaying next question in the array to dom
+function nextQuestion(index) {
+    const currentQuestion = shuffledQuestions[index]
+    document.getElementById("display-question").innerHTML = currentQuestion.question;
+    document.getElementById("answer-one-label").innerHTML = currentQuestion.answer1;
+    document.getElementById("answer-two-label").innerHTML = currentQuestion.answer2;
+    document.getElementById("answer-three-label").innerHTML = currentQuestion.answer3;
+    document.getElementById("answer-four-label").innerHTML = currentQuestion.answer4;
+}
+
+function revealAnswer() {
+    const currentQuestion = shuffledQuestions[indexNumber] //gets current Question 
+    const currentQuestionAnswer = currentQuestion.correctAnswer //gets current Question's answer
+    const answers = document.getElementsByName("answer"); //gets all elements in dom with name of 'answer' (in this the radio inputs)
+    let correctAnswer = null
+
+    answers.forEach((answer) => {
+        if (answer.value === currentQuestionAnswer) {
+            //get's correct's radio input with correct answer
+            correctAnswer = answer.labels[0].id
+        }
+    })
+
+    answers.forEach((answer) => {
+        document.getElementById(correctAnswer).style.backgroundColor = "limegreen"
+    })
+}
+
+function nextQuestionPress() {
+    indexNumber++
+    const answers = document.getElementsByName("answer");
+    answers.forEach((answer) => {
+        document.getElementById(answer.labels[0].id).style.backgroundColor = ""
+    })
+    nextQuestion(indexNumber)
+}
